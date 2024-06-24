0f867f07aa74dfd79a666199dff431303a891319
MariaBelenGallardoPedretti <belengallardop@gmail.com>
2024-06-24 18:49:36 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,58 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const button = document.getElementById("botonContarClicks");
+
+  button.addEventListener("click", event => {
+    button.textContent = `Recuento de clics: ${event.detail}`;
+    excusa();
+  });
+
+  function excusa() {
+    let resultado;
+    let who = ["El caballo", "Mi mamá", "Mi perro", "El loro", "La hormiga"];
+    let action = ["comió", "destruyó", "lanzó", "mordió", "pateó", "rompió"];
+    let what = [
+      "mi ordenador",
+      "mi trabajo",
+      "mis apuntes",
+      "mi teléfono",
+      "mi auto",
+      "mis pantalones"
+    ];
+    let when = [
+      "cuando estaba en el gimnasio",
+      "cuando volvía de pasear",
+      "cuando me estaba bañando",
+      "mientras cocinaba"
+    ];
+    resultado =
+      who[Math.floor(Math.random() * (who.length - 1))] +
+      " " +
+      action[Math.floor(Math.random() * (when.length - 1))] +
+      " " +
+      what[Math.floor(Math.random() * (what.length - 1))] +
+      " " +
+      when[Math.floor(Math.random() * (when.length - 1))];
+
+    document.getElementById("excuse").innerHTML = resultado;
+  }
+  let contadorDeClicks = 0;
+  function contarClicks() {
+    contadorDeClicks++;
+    if (contadorDeClicks > 6) {
+      alert(
+        "¡Has superado las excusas posibles por el día de hoy! ¡A trabajar!"
+      );
+    }
+  }
+
+  document
+    .getElementById("botonContarClicks")
+    .addEventListener("click", excusa);
+  document
+    .getElementById("botonContarClicks")
+    .addEventListener("click", contarClicks);
+
+  return excusa();
 };
